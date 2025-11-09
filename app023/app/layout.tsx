0f7317864e1011df6cc014564c618b9b1735fcfb
takamiya1021 <takamiya1021@gmail.com>
2025-11-09 22:43:44 +0900

    Fix: Add Providers wrapper to layout

--- app023/app/layout.tsx
+++ app023/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 import { VibeKanbanCompanion } from "./vibe-kanban-web-companion";
 
 const geistSans = Geist({
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <VibeKanbanCompanion />
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
